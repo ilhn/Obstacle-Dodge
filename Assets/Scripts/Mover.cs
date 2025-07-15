453528ad02b07636adf0a32667a07c05bc620bb4
ilhan <ilhantopal71@gmail.com>
2025-07-15 17:12:09 +0300

    added movespeed feature

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,10 +9,12 @@ public class Mover : MonoBehaviour
     [SerializeField]
     float zValue = 0f;
 
+    public float moveSpeed = 1.5f;
+
     void Update()
     {
-        xValue = Input.GetAxis("Horizontal");
-        zValue = Input.GetAxis("Vertical");
-        transform.Translate(xValue * Time.deltaTime, yValue * Time.deltaTime, zValue);
+        xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        zValue = Input.GetAxis("Vertical") * moveSpeed  * Time.deltaTime;
+        transform.Translate(xValue, yValue, zValue);
     }
 }
