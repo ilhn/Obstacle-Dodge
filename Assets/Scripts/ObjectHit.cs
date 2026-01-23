a4f69f57593483ccb29ca3e3e1f7385a59777689
ilhan <ilhantopal71@gmail.com>
2026-01-23 04:46:46 +0300

    add Trigger for changing color of obstacles when touching them with the player

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
+        GetComponent<MeshRenderer>().material.color = Color.red;
         Debug.Log("Something hit me");
     }
 }
